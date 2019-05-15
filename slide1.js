b252293c0b013ae148ae3d3ea8c8896136d3a903
surajung <surajung@saramin.co.kr>
2019-05-15 13:39:21 +0900

    javascript로 슬라이드 구현

---
+++ slide1.js
@@ -0,0 +1,28 @@
+// javascript로 슬라이드 구현
+document.body.innerHTML = `<div class="wrap"></div>`
+const $div = document.getElementsByTagName('div')[0]
+
+const frag = document.createDocumentFragment()
+const $slide = document.createElement('div')
+const $container = document.createElement('ul')
+$slide.className = 'slide'
+$container.className = 'slide__container'
+
+const slideData = [1, 2, 3, 4, 5]
+let currentIndex = 0
+$container.style.width = slideData.length * 100 + 'px'
+slideData.forEach((v, i) => {
+    const $li = document.createElement('li')
+    $li.className = 'slide__item'
+    $li.innerText = v
+    $container.appendChild($li)
+})
+$slide.appendChild($container)
+.addEventListener('click', e => {
+    e.preventDefault()
+    currentIndex = (currentIndex + 1) % slideData.length
+    $container.style.left = -100 * currentIndex + 'px'
+})
+
+frag.appendChild($slide)
+$div.appendChild(frag)
